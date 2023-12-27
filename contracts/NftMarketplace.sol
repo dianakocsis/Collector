484b94837c4a72b6bebba5ef3b17a09f5c968d1c
Diana Kocsis <dlk61@pitt.edu>
2023-12-27 00:26:20 -0500

    change nftmarketplace to inftmarketplace

--- contracts/NftMarketplace.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity ^0.8.0;
-
-interface NftMarketplace {
-    function getPrice(address nftContract, uint nftId) external returns (uint);
-    function buy(address nftContract, uint nftId) external payable;
-}
